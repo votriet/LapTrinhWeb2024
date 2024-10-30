65537595f1fe0bc7fc6e9935d472d36184656235
votriet <92041076+votriet@users.noreply.github.com>
2024-10-30 17:05:06 +0700

    làm trang login

---
+++ Frontend/src/containers/Auth/Login.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+import './Login.scss';
+import { FormattedMessage } from 'react-intl';
+//import { handleLoginApi } from '../../services/userService';
+//import e from 'cors';
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  
+  }
+
+  // handleOnChangeUsername = (event) => {
+  //   this.setState({ username: event.target.value })
+  // }
+
+  // handleOnChangePassword = (event) => {
+  //   this.setState({ password: event.target.value })
+  // }
+
+  // handleLogin = async () => {
+  //   this.setState({
+  //     errMessage: ''
+  //   })
+
+  //   try {
+  //     let data = await handleLoginApi(this.state.username, this.state.password);
+  //     if (data && data.errCode !== 0) {
+  //       this.setState({
+  //         errMessage: data.message
+  //       })
+  //     }
+  //     if (data && data.errCode === 0) {
+  //       this.props.userLoginSuccess(data.user);
+  //       console.log('login succeeds');
+  //     }
+  //   } catch (error) {
+  //     if (error.response) {
+  //       if (error.response.data) {
+  //         this.setState({
+  //           errMessage: error.response.data.message
+  //         })
+  //       }
+  //     }
+  //     console.log('test', error.response);
+
+
+
+  //   }
+  // }
+
+
+  // handleShowHidePassword = () => {
+  //   this.setState({
+  //     isShowPassword: !this.state.isShowPassword
+  //   })
+  // }
+
+  // handleKeyDown = (event) => {
+  //   if (event.key === 'Enter') {
+  //     this.handleLogin();
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content row">
+            <div className="col-12 text-login">Login</div>
+            <div className="col-12 form-group login-input">
+              <label>Username</label>
+              <input type="text" className="form-control"
+               
+              />
+            </div>
+
+            <div className="col-12 form-group login-input">
+              <label>Password</label>
+              <div className="custom-input-password">
+                <input className="form-control"
+                 // type={this.state.isShowPassword ? 'text' : 'password'}
+                  placeholder="Enter your password"
+                
+                />
+                {/* <span onClick={() => { this.handleShowHidePassword() }} >
+                 // <i className={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
+                </span> */}
+
+              </div>
+
+            </div>
+            <div className='col-12' style={{ color: 'red' }}>
+              {/* {this.state.errMessage} */}
+            </div>
+
+            <div className="col-12 ">
+              <button className="btn-login" onClick={() => { this.handleLogin() }}>Login</button>
+            </div>
+            <div className="col-12">
+              <span className="forgot-password" >Forgot your password</span>
+            </div>
+            <div className="col-12 text-center mt-3" >
+              <span className="text-other-login">Or Login with:</span>
+            </div>
+            <div className="col-12 social-login" >
+              <i className="fab fa-google-plus-g google"></i>
+              <i className="fab fa-facebook-f facebook"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    language: state.app.language
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    navigate: (path) => dispatch(push(path)),
+    //userLoginSuccess: (userInfor) => dispatch(actions.userLoginSuccess(userInfor))
+    adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+    adminLoginFail: () => dispatch(actions.adminLoginFail()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
