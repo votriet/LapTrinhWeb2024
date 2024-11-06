7e2a6ea5fa67125c1eb5e8fcc869cfbe06a3d8e9
votriet <92041076+votriet@users.noreply.github.com>
2024-11-06 16:05:32 +0700

    chức năng xóa  user

--- Frontend/src/containers/System/ModalUser.js
+++ Frontend/src/containers/System/ModalUser.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { emitter} from "../../utils/emitter";
 class ModalUser extends Component {
 
     constructor(props) {
@@ -13,9 +14,26 @@ class ModalUser extends Component {
             lastName: '',
             address: '',
         }
+        this.listenToEmitter();
     }
+    listenToEmitter(){
+        emitter.on('EVENT_CLEAR_MODAL_DATA',() =>{
+          //reset state
+          this.setState({
+            email:'',
+          password:'',
+          firstName:'',
+          lastName:'',
+          address:'',
+          phoneNumber:''
+          })
+          //console.log('list',data)
+        })
+       }//muoon nghe event dùng emitter.on, fire nó dùng emitter.emit
+    
 
     componentDidMount() {
+         console.log('mounting modal')
     }
 
     toggle = () => {
@@ -32,7 +50,7 @@ class ModalUser extends Component {
         });
     }
 
-    checkValideInput = () => {
+    checkValidateInput = () => {
         let isValid = true;
         let arrInput = ['email', 'password', 'firstName', 'lastName', 'address'];
         for (let i = 0; i < arrInput.length; i++) {
@@ -48,7 +66,7 @@ class ModalUser extends Component {
     }
 
     handleAddNewUser = () => {
-        let isValid = this.checkValideInput();
+        let isValid = this.checkValidateInput();
         if (isValid === true) {
             console.log('check props', this.props)
 
