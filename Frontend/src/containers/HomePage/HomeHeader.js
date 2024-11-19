6d7d30215e7e2ae0e3cd18fbf7dad4c08fbccbcb
votriet <92041076+votriet@users.noreply.github.com>
2024-11-19 17:56:06 +0700

    tiếp tục làm giao diện header trang chủ

--- Frontend/src/containers/HomePage/HomeHeader.js
+++ Frontend/src/containers/HomePage/HomeHeader.js
@@ -7,9 +7,12 @@ class HomeHeader extends Component {
     render() {
 
         return (
+            <React.Fragment>
             <div className='home-header-container'>
                 <div className='home-header-content'>
                 <div className='left-content'>
+                <i className='fas fa-bars'></i>
+                <div className='header-logo'></div>
                             {/* <i className='fas fa-bars'></i>
                             <img className='header-logo' src={logo} onClick={() => this.returnToHome()} /> */}
                         </div>
@@ -77,6 +80,47 @@ class HomeHeader extends Component {
                         {/* </div> */}
                 </div>
             </div>
+            <div className='home-header-banner'>
+                <div className='content-up'>
+                <div className='title1'>NỀn TẢNG Y TẾ</div>
+                <div className='title2'>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                <div className='search'>
+                <i className='fas fa-search'></i>
+                <input type='text' placeholder="Tìm chuyên khoa khám bệnh" />
+                </div>
+                <div className='options'></div>
+                </div>
+                <div className='content-down'>
+                            <div className='options'>
+                                <div className='option-child'>
+                                    <div className='icon-child'><i className='far fa-hospital'></i></div>
+                                    <div className='text-child'>Khám chuyên khoa</div>
+                                </div>
+                                <div className='option-child'>
+                                    <div className='icon-child'><i className='fas fa-mobile-alt'></i></div>
+                                    <div className='text-child'>Khám từ xa</div>
+                                </div>
+                                <div className='option-child'>
+                                    <div className='icon-child'><i className='fas fa-procedures'></i></div>
+                                    <div className='text-child'>khám tổng quát</div>
+                                </div>
+                                <div className='option-child'>
+                                    <div className='icon-child'><i class="fas fa-flask"></i></div>
+                                    <div className='text-child'>Xét nghiệm y học</div>
+                                </div>
+                                <div className='option-child'>
+                                    <div className='icon-child'><i className='fas fa-user-md'></i></div>
+                                    <div className='text-child'>Sức khỏe tinh thần</div>
+                                </div>
+                                <div className='option-child'>
+                                    <div className='icon-child'><i class="fas fa-briefcase-medical"></i></div>
+                                    <div className='text-child'>Khám nha khoa</div>
+                                </div>
+
+                            </div>
+                        </div>
+            </div>
+            </React.Fragment>
         );
     }
 
