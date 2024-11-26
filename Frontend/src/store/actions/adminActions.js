96915e7f0ff9b2b96fa6e4062385038dafc4f4aa
votriet <92041076+votriet@users.noreply.github.com>
2024-11-26 20:59:51 +0700

    làm form tạo người dùng ,hiển thị danh sách user,xóa user

--- Frontend/src/store/actions/adminActions.js
+++ Frontend/src/store/actions/adminActions.js
@@ -1,6 +1,11 @@
 import actionTypes from './actionTypes';
-import { getAllCodeService } from "../../services/userService";
+import { getAllCodeService,
+    createNewUserService ,
+    getAllUsers,
+    deleteUserService
 
+} from "../../services/userService";
+import { toast } from "react-toastify";
 
 // export const fetchGenderStart = () => ({
 //     type: actionTypes.FETCH_GENDER_START
@@ -9,6 +14,9 @@ import { getAllCodeService } from "../../services/userService";
 export const fetchGenderStart = () => {
     return async (dispatch, getState) => {
         try {
+            dispatch({
+                type: actionTypes.FETCH_GENDER_START
+            })
             let res = await getAllCodeService("GENDER");
             if (res && res.errCode === 0) {
                 console.log('check get state', getState);
@@ -33,5 +41,189 @@ export const fetchGenderSuccess = (genderData) => ({
 export const fetchGenderFailed = () => ({
     type: actionTypes.FETCH_GENDER_FAILED
 })
+export const fetchPositionSuccess = (positionData) => ({
+    type: actionTypes.FETCH_POSITION_SUCCESS,
+    data: positionData
+})
+export const fetchPositionFailded = () => ({
+    type: actionTypes.FETCH_POSITION_FAILED
+})
+
+
+export const fetchRoleSuccess = (roleData) => ({
+    type: actionTypes.FETCH_ROLE_SUCCESS,
+    data: roleData
+})
+export const fetchRoleFailded = () => ({
+    type: actionTypes.FETCH_ROLE_FAILED
+})
+export const fetchPositionStart = () => {
+
+    return async (dispatch, getState) => {
+        try {
+
+            let res = await getAllCodeService("POSITION");
+            if (res && res.errCode === 0) {
+                //console.log('hoi dan check',getState)
+                dispatch(fetchPositionSuccess(res.data))
+
+            } else {
+                dispatch(fetchPositionFailded());
+
+            }
+
+        } catch (e) {
+            dispatch(fetchPositionFailded());
+            console.log('fetchPositionStart err', e)
+
+        }
+
+    }
+
+}
+
+export const fetchRoleStart = () => {
+
+    return async (dispatch, getState) => {
+        try {
+
+            let res = await getAllCodeService("ROLE");
+            if (res && res.errCode === 0) {
+
+                //console.log('hoi dan check',getState)
+                dispatch(fetchRoleSuccess(res.data))
+
+            } else {
+                dispatch(fetchRoleFailded());
+
+            }
+
+        } catch (e) {
+            dispatch(fetchRoleFailded());
+            console.log('fetchRoleStart err', e)
+
+        }
+
+    }
+
+}
+
+export const createNewUser = (data) => {
+    return async (dispatch, getState) => {
+        try {
+
+            let res = await createNewUserService(data);
+            
+            if (res && res.errCode === 0) {
+                toast.success("Create a new user succeed!");
+                //console.log('hoi dan check',getState)
+                dispatch(saveUserSuccess())
+                dispatch(fetchAllUsersStart());
+            } else {
+                dispatch(saveUserFailded());
+                toast.error("Duplicate mail");
+
+            }
+
+        } catch (e) {
+            dispatch(saveUserFailded());
+            console.log('saveUserFailed err', e)
+
+        }
+
+    }
+
+
+
+
+}
+
+export const saveUserSuccess = () => ({
+    type: actionTypes.CREATE_USER_SUCCESS
+})
+
+export const saveUserFailded = () => ({
+    type: actionTypes.CREATE_USER_FAILED
+})
+
+export const fetchAllUsersStart = () => {
+
+    return async (dispatch, getState) => {
+        try {
+
+            let res = await getAllUsers("ALL");
+            //let res1 =await  getTopDoctorHomeService(4);
+            //console.log('get top doctor:',res1)
+            if (res && res.errCode === 0) {
+
+                //console.log('hoi dan check',getState)
+                dispatch(fetchAllUsersSuccess(res.users.reverse()))
+
+            } else {
+                toast.error("createa new user errro!");
+                dispatch(fetchAllUsersFailded());
+
+            }
+
+        } catch (e) {
+            toast.error("create a new user errro!");
+            dispatch(fetchAllUsersFailded());
+            console.log('fetchAllUsersFailded err', e)
+
+        }
+
+    }
+
+}
+export const fetchAllUsersSuccess = (data) => ({
+    type: actionTypes.FETCH_ALL_USERS_SUCCESS,
+    users: data
+})
+
+export const fetchAllUsersFailded = () => ({
+    type: actionTypes.FETCH_ALL_USERS_FAILED,
+
+})
+
+export const deleteAUser = (userId) => {
+    return async (dispatch, getState) => {
+        try {
+
+            let res = await deleteUserService(userId);
+
+            if (res && res.errCode === 0) {
+                toast.success("delete a new user succeed!");
+                //console.log('hoi dan check',getState)
+                dispatch(deleteUserSuccess())
+                dispatch(fetchAllUsersStart());
+            } else {
+                toast.error("delete a new user errro!");
+                dispatch(deleteUserFailed());
+
+            }
+
+        } catch (e) {
+            toast.error("delete a new user errro!");
+            dispatch(deleteUserFailed());
+            console.log('deleteUserFailed err', e)
+
+        }
+
+    }
+
+
+
+
+}
+
+export const deleteUserSuccess = () => ({
+    type: actionTypes.DELETE_USER_SUCCESS
+})
+
+export const deleteUserFailed = () => ({
+    type: actionTypes.DELETE_USER_FAILED
+})
+
+
 
 // start doing end
--- Frontend/src/store/reducers/adminReducer.js
+++ Frontend/src/store/reducers/adminReducer.js
@@ -1,34 +1,105 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
+    isloadingGender: false,
     genders: [],
     roles: [],
-    positions: []
+    positions: [],
+    users:[]
 }
 
 const adminReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.FETCH_GENDER_START:
-            console.log('fetch gender start', action)
-
+           let copyState = { ...state };
+            copyState.isloadingGender = true;
+            //console.log('hoi',action)
             return {
-                ...state
-            }
+                ...copyState,
 
+            }
         case actionTypes.FETCH_GENDER_SUCCESS:
-            let copyState = { ...state };
-            copyState.genders = action.data;
-            console.log('fetch gender succ', copyState)
-           
+
+            //let copyState = {...state};
+            state.genders = action.data;
+            state.isloadingGender = false;
+            //console.log('hoi a',action);
+
             return {
-                ...copyState
+
+                ...state,
+
+            }
+        case actionTypes.FETCH_GENDER_FAILED:
+            // console.log('hoi di',action)
+            //let state = {...state};
+
+            state.isloadingGender = false;
+            state.genders = [];
+            return {
+                ...state,
+
             }
 
-        case actionTypes.FETCH_GENDER_FAILED:
-            console.log('fetch gender fail', action)
+        case actionTypes.FETCH_POSITION_SUCCESS:
+
+            //let copyState = {...state};
+            state.positions = action.data;
+            //state.isloadingGender = false;
+            //console.log('hoi a',action);
+
             return {
-                ...state
+
+                ...state,
+
             }
+        case actionTypes.FETCH_POSITION_FAILED:
+            // console.log('hoi di',action)
+            //let state = {...state};
+
+            //state.isloadingGender = false;
+            state.positions = [];
+            return {
+                ...state,
+
+            }
+
+        case actionTypes.FETCH_ROLE_SUCCESS:
+
+            //let copyState = {...state};
+            state.roles = action.data;
+            //state.isloadingGender = false;
+            //console.log('hoi a',action);
+
+            return {
+
+                ...state,
+
+            }
+        case actionTypes.FETCH_ROLE_FAILED:
+            // console.log('hoi di',action)
+            //let state = {...state};
+
+            //state.isloadingGender = false;
+            state.roles = [];
+            return {
+                ...state,
+
+            }
+            case actionTypes.FETCH_ALL_USERS_SUCCESS:
+                state.users = action.users;
+                return {
+                    ...state,
+    
+                }
+    
+            case actionTypes.FETCH_ALL_USERS_FAILED:
+                state.users = [];
+                return {
+                    ...state,
+    
+                }
+        
 
         default:
             return state;
