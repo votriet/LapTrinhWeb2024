5108457d09ef79d0109382a4fa8ad4167f75689a
Truong Thanh Nhan <91606855+nhanthanh1126@users.noreply.github.com>
2024-11-26 07:44:35 +0700

    áp dụng Redux để lưu trữ dữ liệu

---
+++ Frontend/src/store/actions/adminActions.js
@@ -0,0 +1,37 @@
+import actionTypes from './actionTypes';
+import { getAllCodeService } from "../../services/userService";
+
+
+// export const fetchGenderStart = () => ({
+//     type: actionTypes.FETCH_GENDER_START
+// })
+
+export const fetchGenderStart = () => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await getAllCodeService("GENDER");
+            if (res && res.errCode === 0) {
+                console.log('check get state', getState);
+                
+                dispatch(fetchGenderSuccess(res.data));
+            } else {
+                dispatch(fetchGenderFailed());
+
+            }
+        } catch (e) {
+            dispatch(fetchGenderFailed());
+            console.log('fetch gender start error', e);
+        }
+    }
+}
+
+export const fetchGenderSuccess = (genderData) => ({
+    type: actionTypes.FETCH_GENDER_SUCCESS,
+    data: genderData
+})
+
+export const fetchGenderFailed = () => ({
+    type: actionTypes.FETCH_GENDER_FAILED
+})
+
+// start doing end
--- Frontend/src/store/reducers/rootReducer.js
+++ Frontend/src/store/reducers/rootReducer.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import appReducer from "./appReducer";
-// import adminReducer from "./adminReducer";
+import adminReducer from "./adminReducer";
 import userReducer from "./userReducer";
 
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
@@ -38,4 +38,5 @@ export default (history) => combineReducers({
     user: persistReducer(userPersistConfig, userReducer),
     app: persistReducer(appPersistConfig, appReducer),
     // app: appReducer
+    admin: adminReducer
 })
